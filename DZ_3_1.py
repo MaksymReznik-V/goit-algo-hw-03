b22c9d401ef930848a5609244b0742b5d40a85ca
MaksymReznik-V <mov10112007@gmail.com>
2026-04-01 11:15:17 +0300

    Виправив

--- DZ_3_1.py
+++ DZ_3_1.py
@@ -7,12 +7,13 @@ def get_days_from_today(date):
         date_string = datetime.strptime(date, "%Y-%m-%d")
         now_date = datetime.today()
         amount_day = (date_string.date() - now_date.date()).days
+        return amount_day
    except ValueError:
         print("Не вірний формат дати . Введіть дату у форматі 'PPPP-MM-ДД'")
    
-   return amount_day
+
   
-print(get_days_from_today("2026-04-01"))
+print(get_days_from_today("202-04-01"))
 
 
 
